018f4c738d09268520368febac99b832c791c862
JOÃO GUILHERME VAZ <joao_g_vaz@estudante.sesisenai.org.br>
2025-04-02 11:22:23 -0300

    novo script

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-const http = require('http')
-const url = require('url')
-const {soma} = require('./teste')
+const express = require('express')
+const {soma, subtracao, multiplicacao, divisao } = require('./teste')
+
+const server = express()
 
 const PORT = 8000
 
-//criar servidor
-const server = http.createServer((req, res) =>{
-    const urlCapturada = url.parse(req.url, true)
-    const {query} = urlCapturada
-    let resultado = soma(Number(query.a), Number(query.b))
-    res.end(`o valor final é ${resultado}`)
-  if(pathname == '/soma'){
-    resultado = soma(Number(query.a), Number(query.b))
-  } else if (pathname == '/subtracao'){
-    resultado = subtracao(Number(query.a), Number(query.b))
-  } else if (pathname == '/subtracao'){
-    resultado = divisao (Number(query.a), Number(query.b))
-  } else if (pathname == '/subtracao'){
-    resultado = multiplicacao(Number(query.a), Number(query.b))
-  } else {
-    res.statusCode = 404
-    mensagem = 'pagina não encontrada'
-  }
-  res.end(mensagem)
-
+server.get ('/soma', (req,res) => {
+  res.semd('aqui vai o resultado da minha conta')
+})
+server.get ('/subtracao', (req,res) => {
+  res.semd('aqui vai o resultado da minha conta ')
+})
+server.get ('/multiplicacao', (req,res) => {
+  res.semd('aqui vai o resultado da minha conta ')
+})
+server.get ('/multiplicacao', (req,res) => {
+  res.semd('aqui vai o resultado da minha conta ')
 })
 
 server.listen(PORT, ()=> {
-    console.log(`Servidor rodando na porta ${PORT}`)
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
+
+
+
+// //criar servidor
+// const server = http.createServer((req, res) =>{
+//     const urlCapturada = url.parse(req.url, true)
+//     const {query} = urlCapturada
+//     let resultado = soma(Number(query.a), Number(query.b))
+//     res.end(`o valor final é ${resultado}`)
+//   if(pathname == '/soma'){
+//     resultado = soma(Number(query.a), Number(query.b))
+//   } else if (pathname == '/subtracao'){
+//     resultado = subtracao(Number(query.a), Number(query.b))
+//   } else if (pathname == '/divisao'){
+//     resultado = divisao (Number(query.a), Number(query.b))
+//   } else if (pathname == '/multiplicacao'){
+//     resultado = multiplicacao(Number(query.a), Number(query.b))
+//   } else {
+//     res.statusCode = 404
+//     mensagem = 'pagina não encontrada'
+//   }
+//   res.end(mensagem)
+
+// })
